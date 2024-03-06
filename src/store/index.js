c1d75a6b27362857db334693161a999fed679b43
abduazimkh <azimjon8253@gmail.com>
2024-03-06 08:21:38 +0500

    routes and store added

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,22 @@
-const a = 1
+import { createStore } from 'vuex';
+
+// Create a new store instance.
+const store = createStore({
+  state () {
+    return {
+      data: [],
+    }
+  },
+  mutations: {
+    productsData (state, action) {
+      state.data = [...state.data, action]
+    },
+    removeProductsData (state, action) {
+      const data = state.data?.filter((item) => item?.id !== action?.id);
+      state.data = data;
+    }
+  },
+  
+})
+
+export default store;
