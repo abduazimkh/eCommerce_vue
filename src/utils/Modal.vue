65e1a64740becf011b42ee67c4b2bc2788a1e60e
abduazimkh <azimjon8253@gmail.com>
2024-03-28 23:40:40 +0500

    last commit

--- src/utils/Modal.vue
+++ src/utils/Modal.vue
@@ -1,109 +1,113 @@
 <script>
-  export default {
-    props: {
-      isOpen: {
-        type: Boolean,
-        required: true,
-      },
-      isEditOpen: {
-        type: Boolean,
-        required: true,
-      }
+export default {
+  props: {
+    isOpen: {
+      type: Boolean,
+      required: true,
     },
+    isEditOpen: {
+      type: Boolean,
+      required: true,
+    }
   }
+}
 
 </script>
 
 <template>
   <div v-if="isOpen || isEditOpen" class="modal__wrapper">
-    <button  @click="isOpen = false || (isEditOpen = false) " class="close-btn">x</button>
+    <button @click="isOpen = false || (isEditOpen = false)" class="close-btn">x</button>
     <div class="modal">
-      <slot/>
+      <slot />
     </div>
   </div>
   <div v-else></div>
 </template>
 
 <style lang="scss">
-  .modal__wrapper{
-    background-color: #000000d7;
-    width: 100vw;
-    height: 100vh;
-    position: fixed;
-    top: 0;
-    left: 0;
+.modal__wrapper {
+  background-color: #000000d7;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .close-btn {
+    position: absolute;
+    top: 2rem;
+    right: 2rem;
+    font-size: 2rem;
+    width: 50px;
+    height: 50px;
     display: flex;
-    flex-direction: column;
     align-items: center;
     justify-content: center;
-
-    .close-btn{
-      position: absolute;
-      top: 2rem;
-      right: 2rem;
-      font-size: 2rem;
-      width: 50px;
-      height: 50px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      background-color: #fff;
-      border: none;  
-    }
-
-    form{
-      display: flex;
-      flex-direction: column;
-      gap: 1rem;
-      position: relative;
-      height: 100%;
-      input, select{
-        height: 40px;
-        text-indent: 20px;
-        font-size: 1rem;
-        outline: none;
-        border: 1px solid #111;
-        background-color: transparent;
-        &::-webkit-inner-spin-button{
-          display: none;
-        }
-      }
-      input:focus{
-        color: #fff;
-        border: none;
-        box-shadow: inset 0 0 90px 1px rgb(0, 0, 0);
-        &::placeholder{
-          color: #fff;
-        }
-      }
-
-      button{
-        width: 100%;
-        position: absolute;
-        height: 45px;
-        border: none;
-        background-color: #111;
-        color: #fff;
-        font-size: 1rem;
-        left: 0;
-        bottom: 0;
-      }
-      
-
-    }
-
+    background-color: #fff;
+    border: none;
   }
 
-  .modal{
+  form {
     display: flex;
     flex-direction: column;
     gap: 1rem;
-    padding: 2rem;
-    max-width: 500px;
-    width: 100%;
-    max-height: 550px;
+    position: relative;
     height: 100%;
-    background-color: #fff;
+
+    input,
+    select {
+      height: 40px;
+      text-indent: 20px;
+      font-size: 1rem;
+      outline: none;
+      border: 1px solid #111;
+      background-color: transparent;
+
+      &::-webkit-inner-spin-button {
+        display: none;
+      }
+    }
+
+    input:focus {
+      color: #fff;
+      border: none;
+      box-shadow: inset 0 0 90px 1px rgb(0, 0, 0);
+
+      &::placeholder {
+        color: #fff;
+      }
+    }
+
+    button {
+      width: 100%;
+      position: absolute;
+      height: 45px;
+      border: none;
+      background-color: #111;
+      color: #fff;
+      font-size: 1rem;
+      left: 0;
+      bottom: 0;
+    }
+
+
   }
 
+}
+
+.modal {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 2rem;
+  max-width: 500px;
+  width: 100%;
+  max-height: 550px;
+  height: 100%;
+  background-color: #fff;
+}
 </style>
--- src/utils/ProductSwiperImg.vue
+++ src/utils/ProductSwiperImg.vue
@@ -1,48 +1,37 @@
 <script>
-  export default {
-    data () {
-      return {
-  	    errorPlaceholder: false,
-      }
+export default {
+  data() {
+    return {
+      errorPlaceholder: false,
+    }
+  },
+  props: {
+    imgs: {
+      required: true,
     },
-    props: {
-      imgs: {
-        required: true,
-      },
-      active: {
-        require: true
-      },
-      el: {
-        required: true
-      },
-      first: {
-        required: true
-      }
+    active: {
+      required: true,
     },
-    updated(){
-      // 
+    el: {
+      required: true,
     }
   }
+}
 </script>
 
 <template>
-    <img
-    :data-id="el"
-    :onError="(e, s) => {
-			if (!s) {
-				errorPlaceholder = true
-			}
+  <img :data-id="el" :onError="(e, s) => {
+    if (!s) {
+      errorPlaceholder = true
     }
-		"
-		  :src="!imgs || errorPlaceholder ? 
-        `https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png` 
-      : imgs"
-    alt="product image"
-  />
+  }
+    " :src="!imgs || errorPlaceholder ?
+    `https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png`
+    : imgs" alt="product image" />
 </template>
 
 <style>
-  img{
-    width: 100%;
-  }
+img {
+  width: 100%;
+}
 </style>
